29292ef48a937817157cb27c83cad0e8814b4af8
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-04-12 21:39:12 +0700

    Xây dựng Logic phần Header #2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,28 +1,30 @@
 import classNames from "classnames/bind";
-import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+import styles from './AccountItem.module.scss';
 
 import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
+        <Link to={`/${data.nickname}`} className={cx('wrapper')}>
             <Image
                 className={cx('avt')}
-                src="https://avatars.githubusercontent.com/u/139200791?s=400&u=15cb9dcb2b47f557ff086155571e3f645f734f0b&v=4"
-                alt=""
+                src={data.avatar}
+                alt={data.full_name}
             />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    Nguyen Van A
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>Dwaf</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,15 @@ import { HeaderOnly } from '~/components/Layouts'
 // Pages
 import Home from '~/Pages/Home'
 import Following from '~/Pages/Following'
+import Profile from '~/Pages/Profile'
 import Upload from '~/Pages/Upload'
 import Search from '~/Pages/Search'
 
 // Public routes
 export const publicRoutes = [
     { path: '/', component: Home }, 
-    { path: '/following', component: Following }, 
+    { path: '/following', component: Following },
+    { path: '/:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly }, 
     { path: '/search', component: Search, layout: null }, 
 ]
